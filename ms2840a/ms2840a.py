fix bug in stdEvery100kHz calculation in ms2840a.py

ms2840a/ms2840a.py
@@ -911,8 +911,9 @@ def main(mode='FFT',
         stds  = [ np.std(result.amp) for result in results ] # [W]
         neps  = np.multiply(stds, np.sqrt(eff_time)) # [W*sqrt(sec)]
         nPoints100kHz = (int)(1.e+5/freq_binwidth)
-        #ampEvery100kHz = np.
-        stdsEvery100kHz = [ np.std(result.amp[::nPoints100kHz]) if nPoints100kHz>0 else 0. for result in results ] # [W] # NOTE: This calculation is wrong!! Please fix me!
+        n100kHz       = (int)(len(results[0].freq)/nPoints100kHz)
+        ampsEvery100kHz = [ result.amp[:nPoints100kHz*n100kHz].reshape(nPoints100kHz,n100kHz) for result in results ]
+        stdsEvery100kHz = [ np.std(amp, axis=1) if nPoints100kHz>0 else 0. for amp in ampsEvery100kHz ] # [W] NOTE: Need to check
         nepsEvery100kHz  = np.multiply(stds, np.sqrt(eff_time)) # [W*sqrt(sec)]
         f.write(f'# Data statistics\n')
         f.write(f'mean, {np.mean(means):e}, W\n')