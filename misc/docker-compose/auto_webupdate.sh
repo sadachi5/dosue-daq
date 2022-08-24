dcf60aee0cd9ef1ae8694d88120f6f0b39416574
Shunsuke Adachi <sadachi5@gmail.com>
2022-08-24 10:21:42 +0900

    update docker-compose

---
+++ misc/docker-compose/auto_webupdate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# data is synchronized automatically to the local directory
+DATALOCATION=/data
+HTMLLOCATION=/home/adachi/hep_web/grafana
+TEMPERATURE_PLOT=/data/analysis/adachi/dosue-analysis/Temperature/figure/temperature_compare.png
+
+#while [ true ]
+#do
+  echo '### Sart Updating ###';
+  echo "date : `date`";
+  #now=`ssh tandem -C "LANG=C date"`;
+  now=`LANG=C date`;
+  date=`date +%Y-%m-%d`;
+
+  for page in "index.html";
+  do
+    echo "update numbers in $page"
+    ssh hepsrv -C "sed -i \"s%Current values (Update:.*)%Current values (Update:${now})%\" ${HTMLLOCATION}/${page}"
+
+    ## Lakeshore 218 ##
+    echo "$DATALOCATION/lakeshore218/data_${date}.dat";
+    vals=`tail -n 1 $DATALOCATION/lakeshore218/data_${date}.dat`
+    echo $vals;
+    vals=`echo ${vals} | awk -F' |:' '{printf("%s %s:%s:%s %s %.3fK %s %.3fK %s %.3fK %s %.4fK %s %.3fK %s %.3fK %s %.3fK %s %.3fK", $2, $3, $4, $5, $6, $7, $9, $10, $12, $13, $15, $16, $18, $19, $21, $22, $24, $25, $27, $28)}'`
+    echo $vals;
+    ssh hepsrv -C "sed -i \"s%Temperature (Lakeshore 218): .*</li>%Temperature (Lakeshore 218): <br> ${vals} </li>%\" ${HTMLLOCATION}/${page}"
+ 
+    ## CC-10 ##
+    echo "$DATALOCATION/vacuumSensorCC10/data_${date}.dat";
+    vals=`tail -n 1 $DATALOCATION/vacuumSensorCC10/data_${date}.dat`
+    echo $vals;
+    vals=`echo ${vals} | awk -F' |:' '{printf("%s %s:%s:%s %sPa", $2, $3, $4, $5, $7)}'`
+    echo $vals;
+    ssh hepsrv -C "sed -i \"s%Vacuum pressure (CC-10): .*</li>%Vacuum pressure (CC-10): <br>${vals} </li>%\" ${HTMLLOCATION}/${page}"
+
+    ## Temperature plot ##
+    rsync -ruavv $TEMPERATURE_PLOT hepsrv:~/hep_web/grafana/temperature.png
+
+  done
+
+
+#  echo "sleep";
+#  echo "";
+#  echo "";
+#  sleep 60;
+#done
--- misc/docker-compose/grafana/screenshot.py
+++ misc/docker-compose/grafana/screenshot.py
@@ -35,7 +35,7 @@ urls = [
         'http://localhost:3000/d/A7xFebgVz/dosue-y?orgId=1&refresh=10s&from=now-30m&to=now',
         'http://localhost:3000/d/A7xFebgVz/dosue-y?orgId=1&refresh=10s&from=now-1h&to=now',
         'http://localhost:3000/d/A7xFebgVz/dosue-y?orgId=1&refresh=10s&from=now-3h&to=now',
-        'http://localhost:3000/d/A7xFebgVz/dosue-y?orgId=1&refresh=10s&from=1659136800000&to=now',
+        'http://localhost:3000/d/A7xFebgVz/dosue-y?orgId=1&from=1661298480000&to=now',
         ]
 
 # ファイル名接頭辞
@@ -44,56 +44,66 @@ fileNamePrefix = "screenshot"
 # ウインドウ幅指定
 # 初期値: False, スマホ: 375
 # windowSizeWidth = False
-windowSizeWidth = 1200
+windowSizeWidth = 1600
 
 # ウインドウ高さ指定
 # 初期値: False
 #windowSizeHeight = False
-windowSizeHeight = 1000
+windowSizeHeight = 1200
 
 # 連番初期値
 shotNum = 0
 
 # ダウンロードフォルダ作成
 folderCheck = os.path.exists('output')
-if folderCheck:
-  shutil.rmtree('output')
-os.mkdir('output')
+if not folderCheck:
+  os.mkdir('output')
+  pass
 
-# 繰り返し処理
-for i, url in enumerate(urls):
-  # パス指定
-  folderPath = 'output/' + fileNamePrefix + '-' + str(windowSizeWidth) + '-' if windowSizeWidth else 'output/' + fileNamePrefix + '-'
 
-  # サイトURL取得
-  driver.get(url)
-  WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
-  
-  # ウインドウ幅・高さ指定
-  windowWidth = driver.execute_script('return document.body.scrollWidth;')
-  if windowSizeWidth and (windowWidth<windowSizeWidth):
-      windowWidth = windowSizeWidth
-      pass
-  windowHeight = driver.execute_script('return document.body.scrollHeight;')
-  if windowSizeHeight and (windowHeight < windowSizeHeight):
-      windowHeight = windowSizeHeight
-      pass
-  driver.set_window_size(windowWidth, windowHeight)
+try:
 
-  # スクロール処理
-  driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+  # 繰り返し処理
+  for i, url in enumerate(urls):
+    # パス指定
+    folderPath = 'output/' + fileNamePrefix + '-'   
 
-  # 処理後一時待機
-  time.sleep(2)
+    # サイトURL取得
+    driver.get(url)
+    WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+    
+    # ウインドウ幅・高さ指定
+    windowWidth = driver.execute_script('return document.body.scrollWidth;')
+    if windowSizeWidth and (windowWidth<windowSizeWidth):
+        windowWidth = windowSizeWidth
+        pass
+    windowHeight = driver.execute_script('return document.body.scrollHeight;')
+    if windowSizeHeight and (windowHeight < windowSizeHeight):
+        windowHeight = windowSizeHeight
+        pass
+    driver.set_window_size(windowWidth, windowHeight)
+   
+    # スクロール処理
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+   
+    # 処理後一時待機
+    time.sleep(2)
+   
+    # スクリーンショット格納
+    filename = f'{folderPath}{i:02d}.png'
+    print(f'{datetime.datetime.now()} create a screenshot: {filename}')
+    print(f'    from {url}')
+    driver.save_screenshot(filename)
+   
+    # サーバー負荷軽減処理
+    time.sleep(1)
 
-  # スクリーンショット格納
-  filename = f'{folderPath}{i:02d}.png'
-  print(f'{datetime.datetime.now()} create a screenshot: {filename}')
-  print(f'    from {url}')
-  driver.save_screenshot(filename)
+    pass # End of loop
+   
+  # ブラウザ稼働終了
+  driver.quit()
 
-  # サーバー負荷軽減処理
-  time.sleep(1)
-
-# ブラウザ稼働終了
-driver.quit()
+except Exception as e:
+  print(f'Error in getting screenshot for {url}')
+  print(f'Error: {e}')
+  driver.quit()
