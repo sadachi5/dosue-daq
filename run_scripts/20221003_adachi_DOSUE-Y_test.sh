30c930bc7482cfc1f076c4de759ec6ac600fd594
Shunsuke Adachi <sadachi5@gmail.com>
2022-10-07 08:47:37 +0900

    update

--- run_scripts/20221003_adachi_DOSUE-Y_test.sh
+++ run_scripts/20221003_adachi_DOSUE-Y_test.sh
@@ -5,5 +5,9 @@ suffix=$1
 #python3 ../MS2840A/MS2840A.py -f DOSUE-Y_test_230GHz_77K-$1 -m 'SWEEP' -s 3e+9 -w 10e+9 -r 10e+6 -n 10 --att 0 --nRun 1
 
 
-# 77K
-python3 ../MS2840A/MS2840A.py -f DOSUE-Y_test_230GHz_WinScan2-$1 -m 'SWEEP' -s 3e+9 -w 10e+9 -r 10e+6 -n 1 --att 0 --nRun 10
+# window scan
+#python3 ../MS2840A/MS2840A.py -f DOSUE-Y_test_230GHz_WinScan2-$1 -m 'SWEEP' -s 3e+9 -w 10e+9 -r 10e+6 -n 1 --att 0 --nRun 10
+
+
+# at room temperature / NoSIS
+python3 ../MS2840A/MS2840A.py -f DOSUE-Y_test_NoSIS_CISO50ohm_warm$1 -m 'SWEEP' -s 3e+9 -w 10e+9 -r 10e+6 -n 10 --att 0 --nRun 1 --overwrite
