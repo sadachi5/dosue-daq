b44d79fc51c88595ca392e0d019f1f89f291e274
Shunsuke Adachi <sadachi5@gmail.com>
2023-11-02 15:22:02 +0900

    update webcam

--- webcam/runwebcam_getpicture.sh
+++ webcam/runwebcam_getpicture.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/flock -x --timeout 10 /home/dosue/software/dosue-daq/webcam/cronwebcam.lock -c ". /home/dosue/venv/env1/bin/activate; /home/dosue/software/dosue-daq/webcam/webcam_getpicture.py 0 " 2>&1> /home/dosue/software/dosue-daq/webcam/webcam_log.out
+/usr/bin/flock -x --timeout 10 /home/dosue/software/dosue-daq/webcam/cronwebcam.lock -c ". /home/dosue/venv/env1/bin/activate; /home/dosue/software/dosue-daq/webcam/webcam_getpicture.py -u 0 " 2>&1> /home/dosue/software/dosue-daq/webcam/webcam_log.out
